432a5efa2072049f711438a592dc5efe6bcb4053
dmaupin <dm@davidmaupin.com>
2016-02-03 11:48:26 -0800

    sevures routes for both new and edit views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'props#index'
+  authenticate :user do
+  resources :props, only: [:new, :create, :edit, :update, :destroy]
+  end
   resources :props
 end
